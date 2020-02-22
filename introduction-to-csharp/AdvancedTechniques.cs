9a9896ab0f1aab393e337fc01316748af273f9dc
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-02-22 17:48:37 +0000

    Example try catch block for Exception handling.

--- introduction-to-csharp/AdvancedTechniques.cs
+++ introduction-to-csharp/AdvancedTechniques.cs
@@ -9,6 +9,27 @@ namespace introduction_to_csharp.advanced
         public static void Run(string[] args)
         {
 
+            HandleExceptions();
+            Console.ReadLine();
+        }
+
+        static void HandleExceptions()
+        {   try
+            {
+                Console.WriteLine("Enter a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter another number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+                int output = numberOne / numberTwo;
+
+                Console.WriteLine("Ouput: {0}", output);
+            }
+            catch(Exception err) 
+            {
+                Console.WriteLine(err.Message);
+            }
         }
     }
 }
